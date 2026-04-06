3619633941f0d393e242a9dbd7ee93c37f9e9239
janhavicodes11 <janhavi.hariom2026@gmail.com>
2026-04-07 01:54:13 +0530

    Add files via upload

---
+++ Variables, Data Types and Operators/script.js
@@ -0,0 +1,93 @@
+// Variable and declaration in JavaScript
+
+
+//Question 1 :
+//const city= "mumbai"
+//const name= "janhavi"
+//let age=20
+//console.log("Name:", name);
+//console.log("City:", city);
+//console.log("Age:", age);
+
+
+
+
+//Question 2 :
+//let x=5;
+//let x=10;
+//console.log(x); // it throws errors Identifier 'x' has already been declared 
+
+
+
+
+//Question 3:
+//console.log(count);
+//var count = 42; // the output will be undefined 
+
+
+
+//Question 4:
+//const person= {
+    //Name : "janhavi",
+    //city :"mumbai"
+//};
+
+//person.age=20;
+//console.log(person);
+
+//Question 5:
+//let a;
+//console.log(a); // undefined
+
+
+//Question 6: 
+//const array= [1, 2, 3, 4, 5];
+//console.log(array);
+
+
+
+// Data Types practice questions
+// Question 1: 
+//console.log(null + 1); = 1
+//console.log("5" + 3);=53
+//console.log("5" - 3);= 2
+//console.log(true + false);=1
+
+
+
+
+
+// Question 2: 
+//console.log(typeof []); //object
+//console.log(typeof null);// object
+//console.log(typeof 123n);// BigInt
+
+
+
+// Question 3: 
+//function isEmpty(value) {
+//    return value === null || value === undefined || value === "";
+//}
+//console.log(isEmpty(null)); // true
+//console.log(isEmpty(undefined)); // true
+//console.log(isEmpty("")); // true
+//console.log(isEmpty("hello")); // false
+
+
+// Question 4 : 
+//console.log(5 == "5"); // true
+//console.log(5 === "5"); // false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
