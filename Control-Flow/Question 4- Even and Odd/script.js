0560a83a17b08a0f3f9a98e38106e699d03bff72
janhavicodes11 <janhavi.hariom2026@gmail.com>
2026-04-14 16:35:32 +0530

    Add files via upload

---
+++ Control-Flow/Question 4- Even and Odd/script.js
@@ -0,0 +1,13 @@
+let num1= 20; 
+let num2= 15 
+function EvenOdd(num1 , num2){
+    if(num1%2==0){
+        console.log("Num1 is even")
+
+    }
+
+    else{
+        console.log1("Its Odd")
+    }
+}
+console.log (EvenOdd(num1 , num2))
