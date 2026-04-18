b63f856e435ff3283b0938622f8318270f6c5b72
Janhavi <janhavi.hariom2026@gmail.com>
2026-04-18 23:02:24 +0530

    Add files via upload

---
+++ Loops/Question 6- Sum from 1-100/script.js
@@ -0,0 +1,13 @@
+// Print Sum from 1-100 using loops
+// I solved in this way 
+// for(i=1; i<101;i++){
+    //console.log(i+i)
+//}
+
+// Sir way
+let sum=0;
+
+for(let i=1;i<101;i++){
+    sum=sum+i;
+}
+console.log(sum)
