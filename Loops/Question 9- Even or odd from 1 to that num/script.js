a3e3f6f2dd87ac4c9ec0d69ab6fc2785be49c528
Janhavi <janhavi.hariom2026@gmail.com>
2026-04-19 00:08:13 +0530

    Add files via upload

---
+++ Loops/Question 9- Even or odd from 1 to that num/script.js
@@ -0,0 +1,9 @@
+let val=prompt("enter a num :")
+for(let i=1; i<=val;i++){
+    if(i%2===0){
+        console.log(`${i} the number is even`)
+    }
+    else{
+        console.log(`${i} Its odd`)
+    }
+}
